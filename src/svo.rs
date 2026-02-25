8378db17dcffa2e923f4346404f5dd59db9ae02e
Romain Plumaugat <romain.plumaugat@protonmail.com>
2026-02-25 17:49:15 +0100

    add insert fn to the SVO

--- src/svo.rs
+++ src/svo.rs
@@ -12,7 +12,7 @@ pub struct SVO {
 impl SVO {
     pub fn new() -> Self {
         let mut nodes = Vec::with_capacity(1024); // Reserved capacity -> 1kB
-        nodes.push(SVONode::default());
+        nodes.push(SVONode::default()); // The root node
 
         Self { nodes }
     }
@@ -25,5 +25,39 @@ impl SVO {
         }
 
         new_idx
+    }
+
+    pub fn insert(&mut self, coord: glam::UVec3, color: u32, max_depth: u32) {
+        let mut current_node_idx = 0; // Start in the root
+        let mut size = 2_u32.pow(max_depth); // World size in block
+
+        // Work coordinates (begin to world coordinates)
+        let mut cur_coord = coord;
+
+        for depth in 0..max_depth {
+            size /= 2; // Subdivide world
+
+            // Determine the child from the cube position (0-7)
+            let child_x = if cur_coord.x >= size { 1 } else { 0 };
+            let child_y = if cur_coord.y >= size { 1 } else { 0 };
+            let child_z = if cur_coord.z >= size { 1 } else { 0 };
+
+            let child_offset = (child_x << 2) | (child_y << 1) | child_z;
+
+            // Allocate childs in the actual node if required
+            if (self.nodes[current_node_idx].children_idx == 0) {
+                let new_node_idx = self.allocate_children();
+                self.nodes[current_node_idx].children_idx = new_node_idx;
+            }
+
+            // Go down to the next level
+            current_node_idx = (self.nodes[current_node_idx].children_idx + child_offset) as usize;
+
+            // Convert coordinates to local coord for the next level
+            cur_coord %= size;
+        }
+
+        // In the leaf, set the color
+        self.nodes[current_node_idx].color = color;
     }
 }
